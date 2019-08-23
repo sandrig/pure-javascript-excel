c730dc98391b9fb2d8b06534d84502e49b724877
Andrey Savelyev <sandrig.dev@gmail.com>
2019-08-23 22:45:29 +0300

    feat: save content cell (#9)
    * feat: add resizable column and row for table
    
    * refactor: delete unuse code
    
    * feat: add save data table in localStorage
    
    * fix: eslint errors
    
    * refactor: resize column
    
    * refactor: moved rowId to the top
    
    * feat: add save data cell
    
    * refactor: state functions name

--- src/state.js
+++ src/state.js
@@ -1,4 +1,4 @@
-export function saveState(name, key, value) {
+export function saveResizableValue(name, key, value) {
   const state = localStorage.getItem(name)
     ? JSON.parse(localStorage.getItem(name))
     : {};
@@ -6,9 +6,24 @@ export function saveState(name, key, value) {
   localStorage.setItem(name, JSON.stringify(state));
 }
 
-export function getState(name, index, defaultValue) {
+export function getResizableValue(name, index, defaultValue) {
   const state = localStorage.getItem(name)
     ? JSON.parse(localStorage.getItem(name))
     : {};
   return state[index] ? state[index] : defaultValue;
+}
+
+export function saveContentCell(id, value) {
+  const state = localStorage.getItem(id)
+    ? JSON.parse(localStorage.getItem(id))
+    : {};
+  state[id] = value;
+  localStorage.setItem(id, JSON.stringify(state));
+}
+
+export function getContentCell(id) {
+  const state = localStorage.getItem(id)
+    ? JSON.parse(localStorage.getItem(id))
+    : {};
+  return state[id] ? state[id] : '';
 }
