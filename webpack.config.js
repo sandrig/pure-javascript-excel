6fb5baf55b2b8b6a70d76162db3b997088f5547a
sandrig <sandrig.dev@gmail.com>
2019-08-24 09:54:45 +0300

    fix: webpack build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const devConfig = require('./config/webpack.dev.config.js');
+const prodConfig = require('./config/webpack.prod.config.js');
+
 const environment = (process.env.NODE_ENV || 'development').trim();
 
 if (environment === 'development') {
-    module.exports = require('./config/webpack.dev.config.js');
+  module.exports = devConfig;
 } else {
-    console.log('In PROD')
-    module.exports = require('./config/webpack.prod.config.js');
+  module.exports = prodConfig;
 }
