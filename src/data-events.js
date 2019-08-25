635b6f96e63756a947d505c0121c0b7eff14d25d
sandrig <sandrig.dev@gmail.com>
2019-08-25 22:00:45 +0300

    bug: button handler is unstable

--- src/data-events.js
+++ src/data-events.js
@@ -10,4 +10,20 @@ export function initListeners() {
       saveContentCell(id, value);
     }
   });
+
+  document.addEventListener('click', (event) => {
+    const { align } = event.target.dataset;
+
+    if (!align) {
+      return;
+    }
+
+    if (align === 'left') {
+      console.log('Выравнивание по левому краю');
+    } else if (align === 'center') {
+      console.log('Выравнивание по центру');
+    } else if (align === 'right') {
+      console.log('Выравнивание по правому краю');
+    }
+  });
 }
