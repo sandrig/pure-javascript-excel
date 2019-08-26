96fd4621f939b57a795cc64a32bfc810877ffd28
sandrig <sandrig.dev@gmail.com>
2019-08-26 23:21:56 +0300

    bugs: selected cell

--- src/data-events.js
+++ src/data-events.js
@@ -12,18 +12,42 @@ export function initListeners() {
   });
 
   document.addEventListener('click', (event) => {
-    const { align } = event.target.dataset;
+    const { align, type } = event.target.dataset;
+    const el = event.target;
+    const selectedEl = Array.from(
+      document.querySelectorAll('.table__cell--selected'),
+    );
+
+    if (type === 'cell') {
+      selectedEl.length !== 0
+        ? selectedEl.forEach((item) =>
+            item.classList.remove('table__cell--selected'),
+          )
+        : el.classList.add('table__cell--selected');
+    }
+
+    if (event.target.dataset.column) {
+      // console.log(event.target.dataset.column);
+      const columnId = event.target.dataset.column;
+      const selector = `table__cell, [data-column="${columnId}"]`;
+      const columns = Array.from(document.querySelectorAll(selector));
+      columns.forEach((item) => item.classList.add('table__cell--selected'));
+    }
 
     if (!align) {
       return;
     }
 
+    const textAlignment = (position) => {
+      selectedEl.forEach((item) => (item.style.textAlign = position));
+    };
+
     if (align === 'left') {
-      console.log('Выравнивание по левому краю');
+      textAlignment('left');
     } else if (align === 'center') {
-      console.log('Выравнивание по центру');
+      textAlignment('center');
     } else if (align === 'right') {
-      console.log('Выравнивание по правому краю');
+      textAlignment('right');
     }
   });
 }
